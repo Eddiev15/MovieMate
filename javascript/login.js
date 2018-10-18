86028b6627bbe827e4a1d58ca8c683d11546183a
Eddiev15 <eddie.vilchez15@gmail.com>
2018-10-18 15:01:34 -0400

    Update login.js

--- javascript/login.js
+++ javascript/login.js
@@ -1,70 +1,67 @@
-
-
  // Get elements
-          const btnLogin = document.getElementById('btnLogin');
+ const btnLogin = document.getElementById('btnLogin');
 
-(function(){
-    var ui = new firebaseui.auth.AuthUI(firebase.auth());
-
-    var uiConfig = {
-        callbacks: {
-          signInSuccessWithAuthResult: function(authResult, redirectUrl) {
-            // User successfully signed in.
-            // Return type determines whether we continue the redirect automatically
-            // or whether we leave that to developer to handle.
-            return true;
-          },
-          uiShown: function() {
-            // The widget is rendered.
-            // Hide the loader.
-            document.getElementById('loader').style.display = 'none';
-          }
-        },
-       
-         
-        // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
-        signInFlow: 'popup',
-        signInSuccessUrl: 'index.html',
-        signInOptions: [
-          // Leave the lines as is for the providers you want to offer your users.
-          firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-          //firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-          //firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-          firebase.auth.GithubAuthProvider.PROVIDER_ID,
-          firebase.auth.EmailAuthProvider.PROVIDER_ID,
-          firebase.auth().signInAnonymously(),
-          //firebase.auth.PhoneAuthProvider.PROVIDER_ID
-        ],
-        
-       
-
-       // Click event listener
-          btnLogin.addEventListener('click', e => {
-          firebase.auth().signInAnonymously();
-          });
-  
-       // Auth Listener
-          firebase.auth().onAuthStateChanged(firebaseUser => {
-          console.log(firebaseUser);  
-              if(firebaseUser) {
-                  window.location.href = 'index.html';
-           } else {
-         // User is signed out.
-         // ...
+ (function(){
+     var ui = new firebaseui.auth.AuthUI(firebase.auth());
+ 
+     var uiConfig = {
+         callbacks: {
+           signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+             // User successfully signed in.
+             // Return type determines whether we continue the redirect automatically
+             // or whether we leave that to developer to handle.
+             return true;
+           },
+           uiShown: function() {
+             // The widget is rendered.
+             // Hide the loader.
+             document.getElementById('loader').style.display = 'none';
            }
-          };
-     
-              
+         },
         
-        // Terms of service url.
-        tosUrl: 'mmindex.html',
-        // Privacy policy url.
-        privacyPolicyUrl: '<your-privacy-policy-url>'
-      };
-    
+          
+         // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+         signInFlow: 'popup',
+         signInSuccessUrl: 'index.html',
+         signInOptions: [
+           // Leave the lines as is for the providers you want to offer your users.
+           firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+           //firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+           //firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+           firebase.auth.GithubAuthProvider.PROVIDER_ID,
+           firebase.auth.EmailAuthProvider.PROVIDER_ID,
+           firebase.auth().signInAnonymously(),
+           //firebase.auth.PhoneAuthProvider.PROVIDER_ID
+         ],
+         
+        
+ 
+        // Click event listener
+          //  btnLogin.addEventListener('click', e => {
+          //  firebase.auth().signInAnonymously();
+          //  });
+   
+        // Auth Listener
+           firebase.auth().onAuthStateChanged(firebaseUser => {
+           console.log(firebaseUser);  
+               if(firebaseUser) {
+                   window.location.href = 'index.html';
+            } else {
+          // User is signed out.
+          // ...
+            }
+           };
       
-
-      ui.start('#firebaseui-auth-container', uiConfig);
-})()
-
-
+               
+         
+         // Terms of service url.
+         tosUrl: 'mmindex.html',
+         // Privacy policy url.
+         privacyPolicyUrl: '<your-privacy-policy-url>'
+       };
+     
+       
+ 
+       ui.start('#firebaseui-auth-container', uiConfig);
+ })()
+ 
