7512da5b711b0690ee943862613156055a898923
Eddiev15 <eddie.vilchez15@gmail.com>
2018-10-19 15:12:33 -0400

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -21,17 +21,5 @@ firebase.auth().onAuthStateChanged(function(user) {
 
 });
 
-    
-function logOut(){  
-    firebase.initializeApp(settings);
 
-    firebase.auth().signOut().then(function() {
-            sessionStorage.removeItem('tokenK');
-            sessionStorage.removeItem('displayName');
-            sessionStorage.removeItem('userName');
-            window.open('index.html', '_self');                 
-    }).catch(function(error) {
-        console.log(error);
-    }); 
-}
 
